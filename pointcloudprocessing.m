296bab19051b958d31a3ccd3fe427f870f1a4862
HuangLIANG99 <huangliang1122@gmail.com>
2020-05-03 13:15:17 +0200

    First Commit

---
+++ pointcloudprocessing.m
@@ -0,0 +1,33 @@
+% Herzlich Willkommen zum Institutsprojekt im 4. Semester. Aufgabe des
+% Projektes ist die Auseinandersetzung mit verschiedenen
+% Methoden/Möglichkeiten der Punktwolkenverarbeitung, sowie des Erstellens
+% einer grafischen Oberfläche (GUI - von engl. graphical user interface) mit AppDesigner.
+
+
+
+% Punktwolke einlesen
+ 
+
+
+% Heruntertastung
+
+
+% 3D Normalverteiltes Rauschen erstellen und zur Punktwolke hinzufügen
+
+
+% Rauschentfernung
+ 
+
+% affine Transformationen
+
+
+% Normalenbestimmung
+
+
+% Segmentierung
+
+
+% ...Eigene Ideen sind gerne gesehen ;-) 
+
+% Visualisierung
+%scatter3(ptCloud.Location(:,1), ptCloud.Location(:,2), ptCloud.Location(:,3));
